f88857d6871a677d0396a5002cad95e2d8a3d3c7
geoffw <devnull@localhost>
2005-08-02 18:32:47 -0700

    [svn-to-darcs @ 12] Even more tests for malformed configuration files. Improved attribute validation.

--- tests/malformed1/test.sh
+++ tests/malformed1/test.sh
@@ -1,3 +1,7 @@
 PATH=../../:/usr/bin
 env-tool --sh --csh --zsh --outputdir . --debug ./dir1 >& errors1.log
 env-tool --sh --csh --zsh --outputdir . --debug ./dir2 >& errors2.log
+env-tool --sh --csh --zsh --outputdir . --debug ./dir3 >& errors3.log
+env-tool --sh --csh --zsh --outputdir . --debug ./dir4 >& errors4.log
+env-tool --sh --csh --zsh --outputdir . --debug ./dir5 >& errors5.log
+env-tool --sh --csh --zsh --outputdir . --debug ./dir6 >& errors6.log
