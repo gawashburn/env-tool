bbc24b3f5ec2fd82c74a585a73da947cdd0e6e73
geoffw <devnull@localhost>
2005-08-02 18:12:28 -0700

    [svn-to-darcs @ 11] More malformed testing. Improved validation testing during parsing.

--- tests/malformed1/test.sh
+++ tests/malformed1/test.sh
@@ -1,2 +1,3 @@
 PATH=../../:/usr/bin
-env-tool --sh --csh --zsh --outputdir dir2 --debug ./dir1 >& errors1.log
+env-tool --sh --csh --zsh --outputdir . --debug ./dir1 >& errors1.log
+env-tool --sh --csh --zsh --outputdir . --debug ./dir2 >& errors2.log
